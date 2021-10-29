35767f9b5aab3ac7a473be471c203cb5425ef3ad
kakarotto-china <62667391+kakarotto-china@users.noreply.github.com>
2021-10-29 18:17:03 +0800

    Create PrimaryKey.java

---
+++ app-starter/autotable-spring-boot-starter/src/main/java/com/example/model/PrimaryKey.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ */
+
+package com.example.model;
+
+import lombok.Data;
+
+/**
+ * 主键信息
+ * 
+ * @author Jason
+ *
+ */
+@Data
+public class PrimaryKey {
+	/**
+	 * TABLE_CAT String => 表类别（可为 null）
+	 */
+	private String catalog;
+	/**
+	 * TABLE_SCHEME String => 表模式（可为 null）
+	 */
+	private String tableSchem;
+	/**
+	 * TABLE_NAME String => 表名称
+	 */
+	private String tableName;
+	/**
+	 * COLUMN_NAME String => 列名称
+	 */
+	private String columnName;
+	/**
+	 * KEY_SEQ short => 主键中的序列号（值 1 表示主键中的第一列，值 2 表示主键中的第二列）。
+	 */
+	private int keySeq;
+	/**
+	 * PK_NAME String => 主键的名称（可为 null）
+	 */
+	private String pkName;
+	/**
+	 * 是否自增
+	 */
+	private boolean autoincrement;
+}
