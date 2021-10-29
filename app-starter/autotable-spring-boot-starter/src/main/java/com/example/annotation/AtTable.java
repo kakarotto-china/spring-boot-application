e6ca0ce6007aecd8ffede95d4196db37c6740a23
kakarotto-china <62667391+kakarotto-china@users.noreply.github.com>
2021-10-29 18:00:32 +0800

    Create AtTable.java

---
+++ app-starter/autotable-spring-boot-starter/src/main/java/com/example/annotation/AtTable.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ */
+
+package com.example.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * 表信息
+ * 
+ * @author Jason
+ *
+ */
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface AtTable {
+	/**
+	 * 表名称
+	 * 
+	 * @return 表名称
+	 */
+	@AliasFor("name")
+	String value() default "";
+
+	/**
+	 * 表名称
+	 * 
+	 * @return 表名称
+	 */
+	@AliasFor("value")
+	String name() default "";
+
+	/**
+	 * 数据库引擎,针对MySql设立
+	 * 
+	 * @return 数据库引擎
+	 */
+	String engine() default "";
+
+	/**
+	 * 注释
+	 * 
+	 * @return 注释
+	 */
+	String comment() default "";
+
+}
