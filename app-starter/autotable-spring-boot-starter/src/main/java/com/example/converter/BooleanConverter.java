0fa9a4c12f25c3e7baf52612458ee6fdb18baf29
kakarotto-china <62667391+kakarotto-china@users.noreply.github.com>
2021-10-29 18:01:27 +0800

    Create BooleanConverter.java

---
+++ app-starter/autotable-spring-boot-starter/src/main/java/com/example/converter/BooleanConverter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ */
+
+package com.example.converter;
+
+import com.example.model.ColumnInfo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 布尔类型转换
+ * 
+ * @author Jason
+ *
+ */
+@Component
+public class BooleanConverter implements Converter<String, Boolean> {
+
+	private static final String NO = "NO";
+	private static final String YES = "YES";
+	private static final String IS_AUTOINCREMENT = "IS_AUTOINCREMENT";
+
+	@Override
+	public boolean isConverter(Class<?> clazz) {
+		if(clazz== ColumnInfo.class) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public Boolean converter(Class<?> clazz,String columnName, String sourceValue) {
+		if(clazz== ColumnInfo.class) {
+			if(IS_AUTOINCREMENT.equals(columnName)) {
+				if(YES.equals(sourceValue)) {
+					return true;
+				}else if(NO.equals(sourceValue)) {
+					return false;
+				}
+			}
+		}
+		return null;
+	}
+
+}
