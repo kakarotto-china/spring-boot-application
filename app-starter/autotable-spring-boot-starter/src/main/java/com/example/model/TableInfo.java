034896c8096a70e953bcdff89104a44d36424384
kakarotto-china <62667391+kakarotto-china@users.noreply.github.com>
2021-10-29 18:17:14 +0800

    Create TableInfo.java

---
+++ app-starter/autotable-spring-boot-starter/src/main/java/com/example/model/TableInfo.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ */
+
+package com.example.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 表信息
+ * 
+ * @author Jason
+ *
+ */
+@Data
+public class TableInfo {
+	private String catalog;
+	/**
+	 * 表名称
+	 */
+	private String tableName;
+	/**
+	 * 字段名称
+	 */
+	private String comment;
+	/**
+	 * 数据库引擎
+	 */
+	private String engine;
+	/**
+	 * 临时字段
+	 */
+	private ColumnInfo tempColumn;
+	/**
+	 * 字段列表
+	 */
+	private List<ColumnInfo> columns;
+	/**
+	 * 主键列表
+	 */
+	private List<PrimaryKey> primaryKeys;
+}
